e18ab4f52795aac3911504f6d7c9499d924e25e7
Ksvcs726 <james2009@ksvcs.kh.edu.tw>
2025-05-09 13:40:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ try:
 
 except Exception as e:
     st.stop()
-    st.error(f"❌ 無法讀取 Google Sheet，請確認分享權限與網址正確。
-錯誤：{e}")
+    錯誤訊息 = f"❌ 無法讀取 Google Sheet，請確認分享權限與網址正確。\\n錯誤：{e}"
+st.error(錯誤訊息)
 
 
 # === 資料來源 ===
